16fe396ac1b9f934b25458cf14d2330619815a08
caochengkai001 <caochengkai001@ke.com>
2022-08-20 14:26:42 +0800

    model design 部分提交

--- wenwan-data-model/src/main/java/com/wenwan/model/parse/ColumnInfoVo.java
+++ wenwan-data-model/src/main/java/com/wenwan/model/parse/ColumnInfoVo.java
@@ -1,14 +1,18 @@
 package com.wenwan.model.parse;
 
 import com.wenwan.model.request.BaseQuery;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class ColumnInfoVo extends BaseQuery {
     private Long id;
-    private String dbName;
-    private String tableName;
+    private String name;
     private String comment;
+    private String type;
+    private String length;
     private String operator;
     private String operationTime;
 }
