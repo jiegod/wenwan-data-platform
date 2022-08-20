6c9fa472260ebdbd4ba6ea5a31ba0c5e2991783a
caochengkai001 <caochengkai001@ke.com>
2022-08-20 14:36:16 +0800

    model design 部分提交

--- wenwan-data-model/src/main/java/com/wenwan/model/parse/TableInfoVo.java
+++ wenwan-data-model/src/main/java/com/wenwan/model/parse/TableInfoVo.java
@@ -1,11 +1,13 @@
 package com.wenwan.model.parse;
 
 import com.wenwan.model.request.BaseQuery;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class TableInfoVo extends BaseQuery {
     private Long id;
+    @ApiModelProperty("库名")
     private String dbName;
     private String tableName;
     private String comment;
