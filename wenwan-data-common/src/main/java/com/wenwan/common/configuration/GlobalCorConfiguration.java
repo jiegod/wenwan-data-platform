82e54cc487ee90cc12091e2455f732f43ac15d76
wenyongjie <782601203@qq.com>
2022-09-04 22:25:44 +0800

    parse rule insert update delete list

---
+++ wenwan-data-common/src/main/java/com/wenwan/common/configuration/GlobalCorConfiguration.java
@@ -0,0 +1,28 @@
+package com.wenwan.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorConfiguration {
+
+    private CorsConfiguration addCorConfig(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", addCorConfig());
+        return new CorsFilter(source);
+    }
+
+}
