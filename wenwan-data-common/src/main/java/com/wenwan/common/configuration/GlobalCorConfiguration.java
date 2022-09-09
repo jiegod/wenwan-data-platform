21ecaf76733a379b24beadcc42ea2c1f5938ff64
wenyongjie <782601203@qq.com>
2022-09-09 10:18:29 +0800

    init mapper

--- wenwan-data-common/src/main/java/com/wenwan/common/configuration/GlobalCorConfiguration.java
+++ wenwan-data-common/src/main/java/com/wenwan/common/configuration/GlobalCorConfiguration.java
@@ -1,20 +1,25 @@
 package com.wenwan.common.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class GlobalCorConfiguration {
+@Slf4j
+public class GlobalCorConfiguration implements WebMvcConfigurer {
 
     private CorsConfiguration addCorConfig(){
+        log.info("GlobalCorConfiguration");
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addExposedHeader("Authorization");
         return corsConfiguration;
     }
 
@@ -23,6 +28,15 @@ public class GlobalCorConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", addCorConfig());
         return new CorsFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+//          .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
     }
 
 }
